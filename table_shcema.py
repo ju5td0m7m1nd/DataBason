c75a6c731ffb39bdba460199655f591038884f38
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-03 00:05:16 +0800

    table_schema

---
+++ table_shcema.py
@@ -0,0 +1,29 @@
+import pickle
+
+class Table :
+    
+    def __init__(self,tableName, primaryKey, attributeList):
+        self.tableName = tableName
+        self.primaryKey = primaryKey
+        self.attributeList = attributeList
+        self.records = {}
+
+    def Insert(newRecord):
+
+        if CheckVaild :
+
+        else:
+
+            print 
+
+
+
+    # error code
+    # 1 vaild
+    # 2 unknown column
+    # 3 duplicate entry for pk
+    # 4 wrong value assign ( type error )
+    # 5 
+    def CheckVaild(record):
+
+    
