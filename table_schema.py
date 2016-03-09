54845893e457150745392fa87d3ead2098c4657a
ju5td0m7m1nd <franks0059@gmail.com>
2016-03-09 21:01:21 +0800

    add error 2, error 4

--- table_schema.py
+++ table_schema.py
@@ -9,11 +9,19 @@ class Table :
         self.records = {}
 
     def Insert(self,newRecord):
-        print newRecord
-        if self.CheckVaild(newRecord) == 1 :
+        ErrorCode = self.CheckValid(newRecord)
+        print ErrorCode
+        if ErrorCode == 1 :
             self.records[len(self.records)+1] = newRecord
+        elif ErrorCode == 2:
+            print "unknown column"
+        elif ErrorCode == 3:
+            print "duplicate pk"
+        elif ErrorCode == 4:
+            print "type error"
+        elif ErrorCode == 5:
+            print "varchar length incorrect"
         else:
-
             print "error" 
 
     # error code
@@ -23,29 +31,44 @@ class Table :
     # 4 wrong value assign ( type error )
     # 5 varchar length incorrect
 
-    def CheckValid(self,record):
-
+    def CheckValid(self,newRecord):
+        self.newRecord = newRecord
         for attribute in self.attributeList:
-                
-            columnType = attribute['type']
-            columnName = attribute['name']
-            print columnType
-            print columnName
-            print record[columnName]
-            if columnType == 'char':
-                if not type(record[attribute['name']]) is str :
-                    return 4
-            elif columnType == 'int':
-                if not type(record[attribute['name']]) is int :
-                    return 4
-            #check type
-            #if type(record[attribute]) != type(attributeType):
-            #    return 4
+            if not self.CheckTypeError(attribute) == True:
+                return 4
+           
+            if attribute['name'] in self.newRecord:  
+                del self.newRecord[attribute['name']]
+        
+        if not self.CheckUnknownColumn():
+            return 2
 
-            #if
-        return 1         
+    def CheckTypeError(self, attribute):
+        columnType = attribute['type']
+        columnName = attribute['name']
+        if columnName in self.newRecord:    
+            if columnType == 'char':
+                if not type(self.newRecord[columnName]) is str:
+                    return False
+            elif columnType == 'int':
+                if not type(self.newRecord[columnName]) is int:
+                    return False
+        return True
+    
+    def CheckUnknownColumn(self):
+        if len(self.newRecord) > 0 :
+            return False
+        return True
+
 
     def __PrintColumn__(self):
         
         for attribute in self.attributeList:
             print attribute['name'] + ' ' + attribute['type']
+
+t = Table('student','',[{'name':'stuname','type':'char','length':'10'},{'name':'stuid','type':'int','length':''}])
+#Error 4 
+#t.Insert({'stuname':'frank','stuid':'123'})
+
+#Error 2
+#t.Insert({'stuname':'frank','id':'123'})
