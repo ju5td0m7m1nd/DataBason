87abd2a435040748225291c6eb85faf37d84f204
markakisdong <mark840205@gmail.com>
2016-03-14 23:01:24 +0800

    add mute button
    v1.0.0
    
    Sound Effects

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ from database import Database
 from table_schema import Table
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.core.audio import SoundLoader
 from kivy.properties import ObjectProperty
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
@@ -15,21 +16,35 @@ class Display(FloatLayout):
     
     query_in = ObjectProperty()
     data_box = ObjectProperty()
+    sound = ObjectProperty(None, allownone=True)
+
+    def mute(self):
+        if self.sound.volume == 1:
+            self.sound.volume = 0
+        elif self.sound.volume == 0:
+            self.sound.volume = 1
 
     def update(self):
+        if self.sound is None:
+            self.sound = SoundLoader.load("21CenturyFox.mp3")
+        if self.sound.status != 'stop':
+            self.sound.stop()
 
         # clear content avoid multiple listview.
         self.data_box.clear_widgets()
 
         # parse query list.
         query_list = re.split(';', self.query_in.text.strip())
+        if '' in query_list:
+            query_list.remove('')
         # prepare the whole table title
         table_title = Label(font_size=30, size_hint=[1, 0.2], color=[0, 1, 0, 1])
         
         for query in query_list:
             try:
                 self.table = db.processQuery(query)
-                #if db.command == 'create':
+                if db.command == 'create':
+                    self.sound.play()
                 #if db.command == 'insert':
                 table_title.text = self.table.tableName
                 self.error = False
