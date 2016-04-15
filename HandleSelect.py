b14ccbd42ebe51b87f25870c6c35e5da5eba88e4
ju5td0m7m1nd <franks0059@gmail.com>
2016-04-15 21:32:15 +0800

    Trouble Shoouting with quote wait for Douglas

--- HandleSelect.py
+++ HandleSelect.py
@@ -17,14 +17,12 @@ Handle query flow :
 '''
 
 class HandleSelect:
-    query = {}
     '''
     ISSUE : Should we pass the db reference in,
             in order to make share it located in same place
     '''
- 
-    def __init__(self,query,db):
-        self.query = query  
+    returnTables = {} 
+    def __init__(self,db):
         self.db = db   
 
     def loadTable(self,queryFrom):
@@ -45,5 +43,83 @@ class HandleSelect:
                     returnTables[tableName] = db.tables[tableName]
             else:
                 raise RuntimeError ("Select from table which doesn't exist") 
-         
-        return returnTables 
+        self.returnTables = returnTables 
+    
+    def checkWhere(self,queryWhere):
+        logic = queryWhere['logic']
+        condition = []
+        condition.append(queryWhere['term1'])  
+        condition.append(queryWhere['term2'])
+       
+        for c in condition:  
+            if c :
+                exp1 = c['exp1']
+                exp2 = c['exp2']
+                op = c['operator']
+  
+        return self.returnTables
+
+    '''
+    Exp may has these type :
+    1. int
+    2. string
+    3. a.column
+    4. column
+    So we have to determine which type it is
+    '''
+
+    def determineExpression(self,exp):
+        # exp is a string.
+        if '\"' in exp: 
+            return exp.split('\"')[1].split('\"')[0]
+        # exp is number.
+        elif type(exp) is int:
+            return exp
+        # exp with a prefix.
+        elif '.' in exp :
+            prefix = exp.split('.')[0]
+            name = exp.split('.')[1]
+            # Check prefix exist
+            if prefix in self.returnTables:
+                #Check column exist
+                if  name in self.returnTables[prefix].attributeList :
+                    expDict = {}
+                    records = self.returnTables[prefix].records
+                    for row in records:    
+                        expDict[row] = records[row][name]
+                    print records[row][name]
+                    return expDict
+                else:
+                    raise RuntimeError ("Column %s not in Table %s",name,prefix)
+            else :
+                raise RuntimeError ("Table: " + prefix + " doesn't be loaded")
+        else :
+            # No prefix need to check exist in either table a or b.
+            if len(self.returnTables) > 1:
+                # A flag to record if there are more than 1 table have some column.
+                flag = 0
+                tableName = '' 
+                for table in self.returnTables:
+                    if exp in table:
+                        flag = flag + 1 
+                        tableName = table
+                # if flag more than one, raise error
+                if flag > 1 :
+                    raise RuntimeError ("Column %s not distinct",exp)
+                else :
+                    expDict = {}
+                    for row in self.returnTables[tableName]:
+                        expDict[row] = self.returnTables[tableName][exp]
+                return expDict                
+  
+            else :
+                # check column in table
+                for table in self.returnTables :
+                    if exp in table :
+                        # a dictionary contain only {row[pk] : row[exp]} .
+                        expDict = {}
+                        for row in table:
+                            expDict[row] = table[exp]
+                            return expDict
+                    else :
+                        raise RuntimeError ("Column not in table %s", exp)
--- UT_HandleSelect.py
+++ UT_HandleSelect.py
@@ -4,33 +4,61 @@ import HandleSelect
 class TestHandleSelect(unittest.TestCase):
     def setUp(self):
         self.db = database.Database()
+        self.hs = HandleSelect.HandleSelect(self.db)    
         if not len(self.db.tables) > 0:
             self.db.processQuery('CREATE TABLE students (id int primary key, name varchar(20), teacherName varchar(20))') 
-            slef.db.processQuery('CREATE TABLE teachers (id int primary key, name varchar(20))')    
+            self.db.processQuery('CREATE TABLE teachers (id int primary key, name varchar(20))')    
+            self.db.processQuery('insert into students values (1, "frank", "Jason Chang")')      
+            self.db.processQuery('insert into students values (2, "Mark", "John Cena")')      
+            self.db.processQuery('insert into students values (3, "Su4", "Sun Hong Lai")')      
+            self.db.processQuery('insert into students values (4, "Douglas", "Sun Hong Wu")')      
+            self.db.processQuery("insert into teachers values (999, 'Sun Hong Wu')")      
+            self.db.processQuery("insert into teachers values (888, 'Sun Hong Lai')")      
         
-    def test_From(self):
+    def testFrom(self):
         
         #Query without AS
         
         query = {'where': {'term2': {}, 'term1': {'operator': '=', 'exp2': 'teacher', 'exp1': 'teachername'}, 'logic': ''}, 'from': [{'alias': '', 'tableName': 'students'}, {'alias': '', 'tableName': 'teachers'}], 'select': {'aggFn': [], 'fieldNames': ['name', 'teachers.name']}}
         db = self.db
-        hs = HandleSelect.HandleSelect(query,db)    
-        returnTable = hs.loadTable(query['from'])
+        self.hs.loadTable(query['from'])
+        returnTable = self.hs.returnTables
         expectedTable = {'students':db.tables['students'],'teachers':db.tables['teachers'],}
         self.assertEqual( returnTable, expectedTable)
     
-    def test_From_As(self):
+    def testFromAs(self):
         
         # Query with AS
 
         query = {'where': {'term2': {'operator': '>', 'exp2': 1, 'exp1': 'a.id'}, 'term1': {'operator': '=', 'exp2': 'a.teacherName', 'exp1': 'teacher.name'}, 'logic': 'and'}, 'from': [{'alias': '', 'tableName': 'teachers'}, {'alias': 'a', 'tableName': 'students'}], 'select': {'aggFn': [], 'fieldNames': ['a.name', 'teachers.name']}}
         db = self.db
-        hs = HandleSelect.HandleSelect(query,db)    
-        returnTable = hs.loadTable(query['from'])
+        self.hs.loadTable(query['from'])
+        returnTable = self.hs.returnTables
         expectedTable = {'a':db.tables['students'],'teachers':db.tables['teachers'],}
         self.assertEqual( returnTable, expectedTable)
-
+    '''
     def test_Where(self): 
+         
+        query = {'where': {'term2': {}, 'term1': {'operator': '=', 'exp2': 'teacher', 'exp1': 'teachername'}, 'logic': 'and'}, 'from': [{'alias': '', 'tableName': 'students'}, {'alias': '', 'tableName': 'teachers'}], 'select': {'aggFn': [], 'fieldNames': ['name', 'teachers.name']}}
+        hs = self.hs
+        expected = {'students':{3:{'teacherName':'Sun Hong Lai','name':'su4'},4:{'teacherName':'Sun Hong Wu','name':'Douglas'}},'teachers':{888:{'name':"Sun Hong Lai" },999:{'name':"Sun Hong Wu"}}}
+        returnTable = hs.checkWhere(query['where']) 
+        for e in expected:
+            self.assertIn(e, returnTable) 
+            for columns in expected[e]:
+                self.assertIn(e, returnTable)
+                self.assertEqual(expected[e][columns],returnTable[e][columns])
+    '''
+    def test_DetermineExp(self):
+        exp = "teachers.name"
+        expected = {999:'Sun Hong Wu',888:'Sun Hong Lai'}   
+        self.hs.loadTable([{'alias':'','tableName':'teachers'},{'alias':'','tableName':'students'}])
+        returnDict = self.hs.determineExpression(exp)
+        self.assertEqual(len(returnDict),len(expected))
+        for key in returnDict :
+            self.assertIn(key, expected)
+            self.assertEqual(returnDict[key],expected[key])
+         
 
 if __name__ == '__main__' and __package__ is None:
     unittest.main()
